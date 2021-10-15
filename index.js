4f97bdd2c45973fc2b81408da0de0550806b6ac5
Phuoc Phan <phanminh_phuoc@yahoo.com>
2021-10-16 01:48:21 +0700

    second commit

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function menuBarAnimation(x){
 function scrollToContent() {
     document.querySelector('#content').scrollIntoView({behavior: 'smooth'});
   }
+function dropDetail(){
+    document.getElementById("cardDetail").classList.toggle('dropDetail');
+    var elem = document.getElementById('btnDetail');
+    if(elem.value == 'Đóng'){
+        elem.value = 'Chi tiết'
+    }else{
+        elem.value = 'Đóng'
+    }
+}
+function dropDetail2(){
+    document.getElementById("cardDetail2").classList.toggle('dropDetail');
+    var elem = document.getElementById('btnDetail2');
+    if(elem.value == 'Đóng'){
+        elem.value = 'Chi tiết'
+    }else{
+        elem.value = 'Đóng'
+    }
+}
+function dropDetail3(){
+    document.getElementById("cardDetail3").classList.toggle('dropDetail');
+    var elem = document.getElementById('btnDetail3');
+    if(elem.value == 'Đóng'){
+        elem.value = 'Chi tiết'
+    }else{
+        elem.value = 'Đóng'
+    }
+}
+function showContact(){
+    document.getElementById('formContent').classList.toggle('showContact');
+}
